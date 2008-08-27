cf06fdde0ce0d207294f7549a25692c5c96bed97
Géraud Le Falher <daureg@gmail.com>
2008-08-27 18:31:02 +0200

    utilise index.html plutôt que index.xhtml (pour faire plaisir à IE)

--- gen-index.sh
+++ gen-index.sh
@@ -1,15 +1,15 @@
 #find -name index.php -cmin -60|sed 's/^\.\///'|sed 's/\/index\.php$//'|sed 's/index\.php/\./'
 BASE=`pwd`
 echo 'lftp -e "' > do_it
-#for i in ecrits ecrits/feerie ecrits/immor style .  about about/cv tmp archives common plan projets projets/sitemap2map projets/arma projets/arma/plagiat projets/arma/deadline projets/ogre projets/xfce; do
-#	cd $i
-#	php index.php > index.xhtml
-#	echo "put -O $i $i/index.xhtml && " >> $BASE/do_it
-#	cd $BASE
-#done
+for i in ecrits ecrits/feerie ecrits/immor style .  about about/cv tmp archives common plan projets projets/sitemap2map projets/arma projets/arma/plagiat projets/arma/deadline projets/ogre projets/xfce; do
+	cd $i
+	php index.php > index.html
+	echo "put -O $i $i/index.html && " >> $BASE/do_it
+	cd $BASE
+done
 cd common
 for i in 404.php standards.php; do
-	NAME=`basename $i .php`.xhtml
+	NAME=`basename $i .php`.html
 	php $i > $NAME
 	echo "put -O common common/$NAME && " >> $BASE/do_it
 done
