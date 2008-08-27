0520b288529f47798995491f4a11eef7786836b5
Géraud Le Falher <daureg@gmail.com>
2008-08-27 20:18:21 +0200

    mise à jour contenu

--- common/standards.php
+++ common/standards.php
@@ -8,12 +8,14 @@ GéraudSoft | Les standard du web
 <h1>1 - Que sont ces standards du Web ?</h1>
 <p>
 Je ne saurais trop vous conseiller de lire <a class="wikipedia" href="http://fr.wikipedia.org/wiki/Standards_du_Web">
-l'article de Wikipedia</a> à ce sujet. Pour résumer, ce sont un ensemble de normes destinées à fournir
-une manière commune d'organiser des données sur le web
-(<a class="wikipedia" href="http://fr.wikipedia.org/wiki/Hypertext_Markup_Language">(X)HTML)</a>,
-de les présenter (<a class="wikipedia" href="http://fr.wikipedia.org/wiki/Cascading_Style_Sheet">CSS</a>),
-de les décrire (<a class="wikipedia" href="http://fr.wikipedia.org/wiki/Extensible_Markup_Language">XML</a>), de les 
-actualiser (<a class="wikipedia" href="http://fr.wikipedia.org/wiki/Atom">Atom</a>), mais aussi de décrire des images 
+l'article de Wikipedia</a> à ce sujet (et pourquoi pas 
+<a href="http://www.siteduzero.com/tutoriel-3-13629-0-le-w3c-et-les-standards-du-web.html" class="extern">
+celui-là</a>). Pour résumer, ce sont un ensemble de normes destinées à fournir une manière commune d'organiser 
+des données sur le web (<a class="wikipedia" href="http://fr.wikipedia.org/wiki/Hypertext_Markup_Language">
+(X)HTML)</a>, de les présenter (<a class="wikipedia" href="http://fr.wikipedia.org/wiki/Cascading_Style_Sheet">
+CSS</a>), de les décrire (<a class="wikipedia" href="http://fr.wikipedia.org/wiki/Extensible_Markup_Language">
+XML</a>), de les actualiser (<a class="wikipedia" href="http://fr.wikipedia.org/wiki/Atom">Atom</a>), 
+mais aussi de décrire des images 
 (<a class="wikipedia" href="http://fr.wikipedia.org/wiki/Scalable_Vector_Graphics">SVG</a>) ou des formules mathématiques
 (<a class="wikipedia" href="http://fr.wikipedia.org/wiki/MathML">MathML</a>) et <a class="extern" href="http://www.w3.org/TR/">bien d'autres.</a>
 </p>
@@ -59,7 +61,7 @@ consulter et à maintenir, assurant par là sa pérennité.
 </p>
 <h1 id="navigateur">4 - Plusieurs derniers mots sur les navigateurs</h1>
 <p>
-Toute cette théorie technique est à l'&#156;uvre dans le logiciel qui permet d'afficher les pages web,
+Toute cette théorie technique est à l'&oelig;uvre dans le logiciel qui permet d'afficher les pages web,
 le navigateur. On ne peut que conseiller d'éviter Windows Internet Explorer. L'immonde version 6 n'a pas été
 mise à jour ne manière significative pendant près de 6 ans, la version 7, bien meilleure, n'est qu'une
 pâle copie de la concurrence et si Microsoft promet un meilleur support des standards dans la 
--- index.php
+++ index.php
@@ -12,7 +12,7 @@ Bienvenue sur le site de Geraudsoft, découvrez y ce qui a changé depuis votre 
 <h3>23 Juin 2008</h3>
 <p>
 Je n'en suis pas vraiment satisfait mais maintenant qu'elle est finie, vous pouvez lire ma
-<a class="intern" href="ecrits/feerie">seconde nouvelle</a>.
+<a class="intern" href="ecrits/feerie/">seconde nouvelle</a>.
 </p>
 </div>
 
@@ -20,9 +20,9 @@ Je n'en suis pas vraiment satisfait mais maintenant qu'elle est finie, vous pouv
 <h3>4 Mai 2008</h3>
 <ul>
 <li>Le changement de couleur marche aussi avec Internet Explorer (vous devriez vraiment envisager de
-<a href="/common/standards.php#navigateur" class="intern">navigateur</a>).</li>
-<li>Le site a un <a class="intern" href="/plan">plan</a> généré par <a class="intern"
- href="/projets/sitemap2map">ce script.</a></li>
+<a href="common/standards.html#navigateur" class="intern">navigateur</a>).</li>
+<li>Le site a un <a class="intern" href="plan/">plan</a> généré par <a class="intern"
+ href="projets/sitemap2map/">ce script.</a></li>
 </ul>
 </div>
 
@@ -37,18 +37,18 @@ avec le sélecteur sous le menu du site.
 <div class="news">
 <h3>24 Avril 2008</h3>
 <ul>
-<li>Début de la rédaction d'une <a class="intern" href="ecrits/feerie">seconde nouvelle</a>.</li>
+<li>Début de la rédaction d'une <a class="intern" href="ecrits/feerie/">seconde nouvelle</a>.</li>
 <li>Vous pouvez lire le début d'un <a class="intern" href="ecrits/book.pdf">roman avorté</a>.</li>
-<li>Une page qui regroupe mes différents <a class="intern" href="projets">projets</a>.</li>
+<li>Une page qui regroupe mes différents <a class="intern" href="projets/">projets</a>.</li>
 </ul>
 </div>
 
 <div class="news">
 <h3>23 Avril 2008</h3>
 <ul>
-<li>Ajout d'une page sur <a class="intern" href="about">moi-même</a>.</li>
-<li>Ajout d'une page sur <a class="intern" href="projets/xfce">Xfce</a>.</li>
-<li>Ecriture de la page sur <a class="intern" href="common/standards.php">les standards du web</a>.</li>
+<li>Ajout d'une page sur <a class="intern" href="about/">moi-même</a>.</li>
+<li>Ajout d'une page sur <a class="intern" href="projets/xfce/">Xfce</a>.</li>
+<li>Ecriture de la page sur <a class="intern" href="common/standards.html">les standards du web</a>.</li>
 <li>Refonte du menu.</li>
 </ul>
 </div>
@@ -56,10 +56,10 @@ avec le sélecteur sous le menu du site.
 <div class="news">
 <h3>19 Avril 2008</h3>
 <ul>
-<li>Ma première <a class="intern" href="ecrits">nouvelle</a> est désormais disponible au format pdf et texte.</li>
+<li>Ma première <a class="intern" href="ecrits/">nouvelle</a> est désormais disponible au format pdf et texte.</li>
 <li>J'ai mis à jour <a class="extern" href="http://www.wordpress-fr.net/">Wordpress</a> vers la version 2.5 (avec presque 20 jours de retard, c'est mal) (en revanche, il est toujours aussi vide).</li>
-<li>En parlant de mise à jour, le noyau <a class="extern" href="http://kernelnewbies.org/Linux_2_6_25">2.6.25</a> de linux est sorti il y a deux jours.</li> 
-<li>Pour mes dossiers de l'année prochaine, j'ai du rédiger un <a class="intern" href="about/cv.png">cv</a>, si vous avez envie de rigoler.</li>
+<li>En parlant de mise à jour, le noyau <a class="extern" href="http://kernelnewbies.org/Linux_2_6_25/">2.6.25</a> de linux est sorti il y a deux jours.</li> 
+<li>Pour mes dossiers de l'année prochaine, j'ai du rédiger un <a class="intern" href="about/cv/">cv</a>, si vous avez envie de rigoler.</li>
 <li>Je viens de rajouter cette page d'actualité, il était temps.</li>
 </ul>
 </div>
@@ -67,7 +67,7 @@ avec le sélecteur sous le menu du site.
 <div class="news">
 <h3>Mars 2008</h3>
 <p>
-Un nouveau thème sur le site, le moteur <a class="intern" href="projets/ogre">Ogre3d</a>
+Un nouveau thème sur le site, le moteur <a class="intern" href="projets/ogre/">Ogre3d</a>
 </p>
 </div>
 
@@ -75,7 +75,7 @@ Un nouveau thème sur le site, le moteur <a class="intern" href="projets/ogre">O
 <h3>Avant 2008</h3>
 <p>
 Il ne c'est pas passé grand chose avant cette date, verifiez le en lisant les
-<a class="intern" href="archives">archives</a>.
+<a class="intern" href="archives/">archives</a>.
 </p>
 </div>
 	
