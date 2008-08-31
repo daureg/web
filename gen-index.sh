14f59e057a865f862cdd8538f640c20fa39d7a0c
Géraud Le Falher <daureg@gmail.com>
2008-08-31 20:33:48 +0200

    changement d'adresse

--- gen-index.sh
+++ gen-index.sh
@@ -4,14 +4,14 @@ echo 'lftp -e "' > do_it
 for i in ecrits ecrits/feerie ecrits/immor style .  about about/cv tmp archives common plan projets projets/sitemap2map projets/arma projets/arma/plagiat projets/arma/deadline projets/ogre projets/xfce; do
 	cd $i
 	php index.php > index.html
-	echo "put -O $i $i/index.html && " >> $BASE/do_it
+	echo "put -O public_html/$i $i/index.html && " >> $BASE/do_it
 	cd $BASE
 done
 cd common
 for i in 404.php standards.php; do
 	NAME=`basename $i .php`.html
 	php $i > $NAME
-	echo "put -O common common/$NAME && " >> $BASE/do_it
+	echo "put -O public_html/common common/$NAME && " >> $BASE/do_it
 done
 cd $BASE
-echo 'exit" -u u39248933 geraudsoft.info' >> do_it
+echo 'exit" -u daureg daureg.freesitespace.net' >> do_it
