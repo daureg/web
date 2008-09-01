45e8aedc562c1018210f00a6328246389c6ee933
Géraud Le Falher <daureg@gmail.com>
2008-09-01 21:14:00 +0200

    enlève le repertoir style des upload automatique

--- gen-index.sh
+++ gen-index.sh
@@ -1,10 +1,10 @@
 #find -name index.php -cmin -60|sed 's/^\.\///'|sed 's/\/index\.php$//'|sed 's/index\.php/\./'
 BASE=`pwd`
 echo 'lftp -e "' > do_it
-for i in ecrits ecrits/feerie ecrits/immor style about about/cv tmp archives common plan projets projets/sitemap2map projets/arma projets/arma/plagiat projets/arma/deadline projets/ogre projets/xfce; do
+for i in ecrits ecrits/feerie ecrits/immor . about about/cv tmp archives common plan projets projets/sitemap2map projets/arma projets/arma/plagiat projets/arma/deadline projets/ogre projets/xfce; do
 	cd $i
 	php index.php > index.html
-	echo "mkdir public_html/$i && put -O public_html/$i $i/index.html && " >> $BASE/do_it
+	echo "put -O public_html/$i $i/index.html && " >> $BASE/do_it
 	cd $BASE
 done
 cd common
