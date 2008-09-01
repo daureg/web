b46c74d7a70a31e88d5ff21b22d7298e02f58428
Géraud Le Falher <daureg@gmail.com>
2008-09-01 21:08:52 +0200

    encore un changement d'adresse

--- gen-index.sh
+++ gen-index.sh
@@ -1,10 +1,10 @@
 #find -name index.php -cmin -60|sed 's/^\.\///'|sed 's/\/index\.php$//'|sed 's/index\.php/\./'
 BASE=`pwd`
 echo 'lftp -e "' > do_it
-for i in ecrits ecrits/feerie ecrits/immor style .  about about/cv tmp archives common plan projets projets/sitemap2map projets/arma projets/arma/plagiat projets/arma/deadline projets/ogre projets/xfce; do
+for i in ecrits ecrits/feerie ecrits/immor style about about/cv tmp archives common plan projets projets/sitemap2map projets/arma projets/arma/plagiat projets/arma/deadline projets/ogre projets/xfce; do
 	cd $i
 	php index.php > index.html
-	echo "put -O public_html/$i $i/index.html && " >> $BASE/do_it
+	echo "mkdir public_html/$i && put -O public_html/$i $i/index.html && " >> $BASE/do_it
 	cd $BASE
 done
 cd common
@@ -14,4 +14,4 @@ for i in 404.php standards.php; do
 	echo "put -O public_html/common common/$NAME && " >> $BASE/do_it
 done
 cd $BASE
-echo 'exit" -u daureg daureg.freesitespace.net' >> do_it
+echo 'exit" -u slyxi slyxi.xf-s.com' >> do_it
