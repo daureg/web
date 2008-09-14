b04dab4ec5ae11dd60748190b7ea45df3b452b33
Géraud Le Falher <daureg@gmail.com>
2008-09-14 15:55:33 +0200

    suppression des paquets de ogre pour gutsy

--- projets/ogre/index.php
+++ projets/ogre/index.php
@@ -11,20 +11,12 @@ Pour découvrir le moteur <a class="extern" href="http://www.ogre3d.org/">Ogre 3
 <p>La version actuelle est la 0.0.1. Vous pouvez :</p>
 <ul>
 <li>Lire <a class="intern" href="NEWS">les nouveautés</a>.</li>
-<li>Voir quels sont <a class="intern" href="TODO">les plans</a>.</li>
+<li>Voir quels sont <a class="intern" href="TODO">les plans</a> (mais ne vous faites pas d'illusion).</li>
 <li>Savoir comment <a class="intern" href="INSTALL">installer</a> à partir des sources.</li>
 <li>Télécharger les sources : <a class="intern" href="ogre-tuto-0.0.1.tar.bz2">ogre-tuto-0.0.1.tar.bz2 [2,2 Mo]</a>
 (md5 : ab854f00cb6150cc8dd2aa5a3dd43366).</li>
 <li>Télécharger les exécutables Windows : <a class="intern" href="ogre-tuto-0.0.1-win32.zip">
 ogre-tuto-0.0.1-win32.zip [7,0 Mo]</a></li>
-<li>Utiliser un paquet debian (testé sous gutsy/i686), rajouter
-<br /><code class="code">
-deb http://www.geraudsoft.info/debian unstable main
-</code><br />
- à votre fichier <samp>/etc/apt/sources.list</samp> et faire
-<br /><code class="code">
-apt-get update &amp;&amp; apt-get install ogre-tuto
-</code></li>
 <li>Si jamais vous utilisez <a class="extern" href="http://archlinuxfr.org/">Archlinux</a>,
 vous pouvez vous servir de ce fichier <a class="intern" href="PKGBUILD">PKGBUILD</a>.</li>
 </ul>
